791c3d22025345d9236ce26c1afb92317231098d
Hasib Ar Rafiul Fahim <fahad.com66.fk@gmail.com>
2021-11-12 01:11:36 +0600

    email fixed

--- ots/accounts/views.py
+++ ots/accounts/views.py
@@ -3,19 +3,55 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib import messages
+from validate_email import validate_email
+from .models import User
 # Create your views here.
 
 
 def signup_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('articles:list')
-    else:
-        form = UserCreationForm()
-    return render(request, 'accounts/signup.html', {'form': form})
+        context = {'has_error': False, 'data': request.POST}
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+
+        if len(password)<6:
+            messages.add_message(request, messages.ERROR, 'Password should be at least 6 character')
+            context['has_error']=True
+
+        if password != password2:
+            messages.add_message(request, messages.ERROR, 'Passwords should be same')
+            context['has_error'] = True
+
+        if not validate_email(email):
+            messages.add_message(request, messages.ERROR, 'Email not valid')
+            context['has_error'] = True
+
+        if not username:
+            messages.add_message(request, messages.ERROR, 'Username is required')
+            context['has_error'] = True
+
+        if User.objects.filter(username=username).exists():
+            messages.add_message(request, messages.ERROR, 'Username already exists')
+            context['has_error'] = True
+
+        if User.objects.filter(email=email).exists():
+            messages.add_message(request, messages.ERROR, 'Email already exists')
+            context['has_error'] = True
+
+        if context['has_error']:
+            return render(request, 'accounts/signup.html', context)
+
+        user=User.objects.create_user(username=username, email=email)
+        user.set_password(password)
+        user.save()
+
+        messages.add_message(request, messages.ERROR, 'Account Created')
+        messages.add_message(request, messages.ERROR, 'You can login now')
+        return redirect('../login/')
+
+    return render(request, 'accounts/signup.html')
 
 
 def login_view(request):
--- ots/ots/settings.py
+++ ots/ots/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL="accounts.User"
+
 
 # Application definition
 
