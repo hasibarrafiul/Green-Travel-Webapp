b1abef32a034cea1516f8c43090e2b4a07645ece
Hasib Ar Rafiul Fahim <fahad.com66.fk@gmail.com>
2021-11-16 02:40:41 +0600

    admin panel

--- ots/ots/settings.py
+++ ots/ots/settings.py
@@ -33,6 +33,8 @@ AUTH_USER_MODEL="accounts.User"
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
