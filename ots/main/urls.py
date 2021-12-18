65e0cfeeb35ae8c9e2a799afbeb76d96d23edc7b
Hasib Ar Rafiul Fahim <fahad.com66.fk@gmail.com>
2021-12-19 03:28:34 +0600

    hotel reservation

--- ots/main/urls.py
+++ ots/main/urls.py
@@ -10,14 +10,15 @@ urlpatterns = [
     url(r'^$', views.homepage, name="list"),
     url(r'^about/$', views.about, name="about"),
     url(r'^contact/$', views.contact, name="contact"),
-    url(r'^ticket/$', views.ticket, name="ticket"),
-    url(r'^ticket_page/$', views.ticket_page, name="ticket_page"),
-    url(r'^hotel_booking/$', views.hotel_booking, name="hotel_booking"),
     url(r'^hotelReview/$', views.hotelReview, name="hotelReview"),
     url(r'^hotelReviewShow/$', views.hotelReviewShow, name="hotelReviewShow"),
     path('deleteHotelReview/<str:pk>/$', views.deleteHotelReview, name="deleteHotelReview"),
     url(r'^resturantReview/$', views.resturantReview, name="resturantReview"),
     url(r'^resturantReviewShow/$', views.resturantReviewShow, name="resturantReviewShow"),
     path('deleteResturantReview/<str:pk>/$', views.deleteresturantReview, name="deleteResturantReview"),
+
+
+    url(r'^hotel_booking/$', views.hotel_booking, name="hotel_booking"),
+    path('hotel_bookingPdf', views.hotel_bookingPdf, name="hotel_bookingPdf"),
 
 ]
--- ots/main/views.py
+++ ots/main/views.py
@@ -6,7 +6,6 @@ from django.template import loader
 from .models import HotelReview
 from .models import ResturantReview
 
-
 from django.contrib.auth.decorators import login_required
 
 from django.http import FileResponse
@@ -14,6 +13,7 @@ import io
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
 from reportlab.lib.pagesizes import letter
+
 from django.core.mail import send_mail
 from . import forms
 
@@ -38,93 +38,17 @@ def contact(request):
         message_phone = request.POST['message-phone']
         message = request.POST['message-message']
 
-        #send email
+        # send email
         send_mail(
-            'Mail Sent By '+message_name , # subject
-            '\n'+'Senders Phone: '+message_phone + '\nSenders Email: '+message_email+ ' \nMessage: ' + message, #message
-            message_email, #from mail
-            ['rashikbuksh71@gmail.com'], #tomail
+            'Mail Sent By ' + message_name,  # subject
+            '\n' + 'Senders Phone: ' + message_phone + '\nSenders Email: ' + message_email + ' \nMessage: ' + message,
+            # message
+            message_email,  # from mail
+            ['rashikbuksh71@gmail.com'],  # tomail
         )
         return render(request, 'main/contact.html', {'message_name': message_name})
     else:
         return render(request, 'main/contact.html')
-
-
-@login_required(login_url="/account/login/")
-def ticket_page(request):
-    return render(request, 'main/ticket_page.html')
-
-
-@login_required(login_url="/account/login/")
-def ticket_page(request):
-    ticket_page.message_name = request.POST.get('message-name')
-    ticket_page.message_email = request.POST.get('message-email')
-    ticket_page.message_phone = request.POST.get('message-phone')
-    ticket_page.message_start = request.POST.get('message-start')
-    ticket_page.message_end = request.POST.get('message-end')
-    ticket_page.message_date = request.POST.get('message-date')
-    ticket_page.message_bus = request.POST.get('message-bus')
-    ticket_page.text = request.POST.get('message-text')
-
-    return render(request, 'main/ticket_page.html', {'message_name': ticket_page.message_name})
-
-
-@login_required(login_url="/account/login/")
-def ticket(request):
-    buf = io.BytesIO()
-    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
-    textob = c.beginText()
-    textob.setTextOrigin(inch, inch)
-    textob.setFont("Helvetica", 14)
-    name = ticket_page.message_name
-    mail = ticket_page.message_email
-    phone = ticket_page.message_phone
-    start = ticket_page.message_start
-    end = ticket_page.message_end
-    date = ticket_page.message_date
-    bus = ticket_page.message_bus
-    text = ticket_page.text
-    # print(text)
-
-    lines = [
-        "Welcome to optimal transportation system",
-        " ",
-        "Your ticket is below",
-        " ",
-        "Full name: " + name,
-        " ",
-        "Email: " + mail,
-        " ",
-        "Phone: "+phone,
-        " ",
-        "Pickup place: "+start,
-        " ",
-        "Destination: "+end,
-        " ",
-        "Journey Date: "+date,
-        " ",
-        "Bus: "+bus,
-        " ",
-        ""+text,
-        "",
-        "All right reserved by optimal transportation system",
-
-    ]
-
-    for line in lines:
-        textob.textLine(line)
-
-    c.drawText(textob)
-    c.showPage()
-    c.save()
-    buf.seek(0)
-
-    return FileResponse(buf, as_attachment=True, filename="ticket.pdf")
-
-
-@login_required(login_url="/account/login/")
-def hotel_booking(request):
-    return render(request, 'main/HotelBooking.html')
 
 
 @login_required(login_url="/account/login/")
@@ -181,3 +105,70 @@ def deleteresturantReview(request, pk):
     instance = ResturantReview.objects.get(id=pk)
     instance.delete()
     return redirect('articles:resturantReviewShow')
+
+
+@login_required(login_url="/account/login/")
+def hotel_booking(request):
+    hotel_booking.user_name = request.POST.get('user-name')
+    hotel_booking.user_email = request.POST.get('user-email')
+    hotel_booking.user_phone = request.POST.get('user-phone')
+    hotel_booking.checkin_date = request.POST.get('checkin-date')
+    hotel_booking.checkout_date = request.POST.get('checkout-date')
+    hotel_booking.hotel_name = request.POST.get('hotel-name')
+    hotel_booking.room_numbers = request.POST.get('room-numbers')
+    hotel_booking.room_type = request.POST.get('room-type')
+
+    return render(request, 'main/hotel_booking.html', {'user_name': hotel_booking.user_name})
+
+
+def hotel_bookingPdf(request):
+    buf = io.BytesIO()
+    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+    textob = c.beginText()
+    textob.setTextOrigin(inch, inch)
+    textob.setFont("Helvetica", 14)
+
+    name = hotel_booking.user_name
+    mail = hotel_booking.user_email
+    phone = hotel_booking.user_phone
+    checkin = hotel_booking.checkin_date
+    checkout = hotel_booking.checkout_date
+    hotel_name = hotel_booking.hotel_name
+    room_number = hotel_booking.room_numbers
+    room_type = hotel_booking.room_type
+
+    lines = [
+        "Welcome to Green Travel",
+        " ",
+        "Your Reservation confirmation is below",
+        " ",
+        "Full name: " + name,
+        " ",
+        "Email: " + mail,
+        " ",
+        "Phone: " + phone,
+        " ",
+        "Check-in date: " + checkin,
+        " ",
+        "Check-out date: " + checkout,
+        " ",
+        "Hotel name: " + hotel_name,
+        " ",
+        "Total number of rooms: " + room_number,
+        " ",
+        "Room type: " + room_type,
+        "",
+        "Thank you for using Green Travel",
+        "All right reserved by Green Travel",
+
+    ]
+
+    for line in lines:
+        textob.textLine(line)
+
+    c.drawText(textob)
+    c.showPage()
+    c.save()
+    buf.seek(0)
+
+    return FileResponse(buf, as_attachment=True, filename='room.pdf')
