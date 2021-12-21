f3a56dc27eae2887af8652e3d2940d0e0bbd3dca
Hasib Ar Rafiul Fahim <fahad.com66.fk@gmail.com>
2021-12-21 18:02:39 +0600

    place merged

--- ots/main/admin.py
+++ ots/main/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 from django.contrib import admin
-from .models import HotelReview, RoomModel, Place
+from .models import HotelReview, RoomModel, PlaceReview, Place
 from .models import ResturantReview
 
 admin.site.register(HotelReview)
 admin.site.register(ResturantReview)
+admin.site.register(PlaceReview)
 admin.site.register(RoomModel)
 admin.site.register(Place)
