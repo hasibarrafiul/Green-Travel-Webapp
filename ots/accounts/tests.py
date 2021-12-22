b9fbb8aaebd569d7005b3cc34a8efa3dc9d11fec
Hasib Ar Rafiul Fahim <fahad.com66.fk@gmail.com>
2021-12-23 00:53:41 +0600

    unit test

--- ots/accounts/tests.py
+++ ots/accounts/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from multiprocessing.connection import Client
 
-# Create your tests here.
+from django.test import SimpleTestCase, TestCase, client
+from django.urls import reverse, resolve
+from .models import User
+import json
+from .views import signup_view, login_view, logout_view, \
+        activate_user
+
+
+class UrlsTest(SimpleTestCase):
+    def test_signup(self):
+        url = reverse('accounts:signup')
+        self.assertEquals(resolve(url).func, signup_view)
+
+    def test_login(self):
+        url = reverse('accounts:login')
+        self.assertEquals(resolve(url).func, login_view)
+
+    def test_logout(self):
+        url = reverse('accounts:logout')
+        self.assertEquals(resolve(url).func, logout_view)
+
+    def test_activateUser(self):
+        url = reverse('accounts:activate', args=['user', 'token'])
+        self.assertEquals(resolve(url).func, activate_user)
+
+
+class ViewTest(TestCase):
+    def test_login_view_POST(self):
+        response = self.client.post(reverse('accounts:login'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/login.html')
+
+    def test_logout_view_POST(self):
+        response = self.client.post(reverse('accounts:logout'))
+        self.assertEquals(response.status_code, 302)
--- ots/main/tests.py
+++ ots/main/tests.py
@@ -1,3 +1,83 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import homepage, about, contact, hotelReview, deleteHotelReview, resturantReview, deleteresturantReview, placeReview, \
+    deleteplaceReview, hotel_booking, hotel_bookingPdf, RoomShow, hotel_page, placelist, place, resturantList, resturantShow
 
-# Create your tests here.
+
+class UrlsTest(SimpleTestCase):
+    def test_homepage(self):
+        url = reverse('articles:list')
+        self.assertEquals(resolve(url).func, homepage)
+
+    def test_about(self):
+        url = reverse('articles:about')
+        self.assertEquals(resolve(url).func, about)
+
+    def test_contact(self):
+        url = reverse('articles:contact')
+        self.assertEquals(resolve(url).func, contact)
+
+    def test_hotelReview(self):
+        url = reverse('articles:hotelReview')
+        self.assertEquals(resolve(url).func, hotelReview)
+
+    def test_deleteHotelReview(self):
+        url = reverse('articles:deleteHotelReview', args=['slug'])
+        self.assertEquals(resolve(url).func, deleteHotelReview)
+
+    def test_resturantReview(self):
+        url = reverse('articles:resturantReview')
+        self.assertEquals(resolve(url).func, resturantReview)
+
+    def test_deleteresturantReview(self):
+        url = reverse('articles:deleteResturantReview', args=['slug'])
+        self.assertEquals(resolve(url).func, deleteresturantReview)
+
+    def test_placeReview(self):
+        url = reverse('articles:placeReview')
+        self.assertEquals(resolve(url).func, placeReview)
+
+    def test_deleteplaceReview(self):
+        url = reverse('articles:deleteplaceReview', args=['slug'])
+        self.assertEquals(resolve(url).func, deleteplaceReview)
+
+    def test_hotel_booking(self):
+        url = reverse('articles:hotel_booking')
+        self.assertEquals(resolve(url).func, hotel_booking)
+
+    def test_hotel_bookingPdf(self):
+        url = reverse('articles:hotel_bookingPdf')
+        self.assertEquals(resolve(url).func, hotel_bookingPdf)
+
+    def test_RoomShow(self):
+        url = reverse('articles:hotel_page')
+        self.assertEquals(resolve(url).func, hotel_page)
+
+    def test_hotel_page(self):
+        url = reverse('articles:RoomShow')
+        self.assertEquals(resolve(url).func, RoomShow)
+
+    def test_placelist(self):
+        url = reverse('articles:placelist')
+        self.assertEquals(resolve(url).func, placelist)
+
+    def test_place(self):
+        url = reverse('articles:place')
+        self.assertEquals(resolve(url).func, place)
+
+    def test_resturantList(self):
+        url = reverse('articles:resturantList')
+        self.assertEquals(resolve(url).func, resturantList)
+
+    def test_resturantShow(self):
+        url = reverse('articles:resturantShow')
+        self.assertEquals(resolve(url).func, resturantShow)
+
+
+class ViewTest(SimpleTestCase):
+    def test_homepage(self):
+        response = self.client.get(reverse('articles:list'))
+        self.assertEquals(response.status_code, 302)
+
+
+
