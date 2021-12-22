620dfa5d172496c5584205840fc1a0a337d6d044
Fahadul Islam <fahadulislam.user@outlook.com>
2021-12-22 20:58:52 +0600

    chat bot implemented

--- ots/assets/home/js/main.js
+++ ots/assets/home/js/main.js
@@ -79,3 +79,14 @@
      new WOW().init();
      
  });
+
+
+    (function(d, m){
+        var kommunicateSettings = 
+            {"appId":"14a9cf733c320c5453296abee0b321c07","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+
