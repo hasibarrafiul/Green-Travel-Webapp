81c3ede0bab9cf26a1e75df36abc7b24251c7b6e
Hasib Ar Rafiul Fahim <fahad.com66.fk@gmail.com>
2021-12-25 17:57:53 +0600

    reservation design

--- ots/main/views.py
+++ ots/main/views.py
@@ -321,3 +321,9 @@ def update_profile(request):
     context = {}
     context['profile'] = profile
     return render(request, 'main/update_user_profile.html', context)
+
+
+@login_required(login_url="/account/login/")
+def reservationnew(request):
+    return render(request, 'main/reservationnew.html')
+
