4ca717014e1132bedf1eabd6fb071608fcbc111d
gl1tchy_af <fahad.com66.fk@gmail.com>
2022-06-23 19:43:17 +0600

    heroku push

--- ots/ots/settings.py
+++ ots/ots/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
